0055d85c551dee59721079f36f7f901a23d3e601
Mikhail Kinchin <mikhail.kinchin@gmail.com>
2021-03-20 00:51:16 -0700

    Add (return back) ability to slide around pictures without magnifying them

--- js/common.js
+++ js/common.js
@@ -15,7 +15,17 @@ function getItemId(url) {
 
 function isBoardPage() {
   var regexp = new RegExp(board_parg_url_regexp);
-  return window.location.toString().match(regexp) != null;
+  return window.location.href.match(regexp) != null;
+}
+
+function isListOfBoardsPage() {
+  var regexp = new RegExp(board_list_page_url);
+  return window.location.pathname.match(regexp) != null;
+}
+
+function isDetailPage() {
+  var regexp = new RegExp(detail_page_url);
+  return window.location.pathname.match(regexp) != null;
 }
 
 function removeFromBoard(e) {
@@ -35,6 +45,8 @@ function removeFromBoard(e) {
 var remove_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path fill="#FFFFFF" d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/></svg>';
 
 var board_parg_url_regexp = /\/board\/([\d\w]{8}\-[\d\w]{4}\-[\d\w]{4}\-[\d\w]{4}\-[\d\w]{12}|quicksaveboard_[\d]*)\//;
+var detail_page_url = /\/item\/detail\/[\d]+[\/]*/;
+var board_list_page_url = /\/boards[\/]*/;
 
 var removeItemFromBoardApiUrl = "https://offerup.com/webapi/offer_boards/v1/boards/{board_id}/items/{item_id}/";
 var removeBoardApiUrl = "https://offerup.com/webapi/offer_boards/v1/boards/{board_id}/";
--- js/content.js
+++ js/content.js
@@ -16,7 +16,7 @@ if(isBoardPage()) {
   });
 }
 
-else if("https://offerup.com/boards/" == window.location) {
+else if(isListOfBoardsPage()) {
   $("div.container-fluid > div.row > a[href*=\"/board/\"] > div").each(function() {
     var board_id = getBoardId($(this).parent().attr("href"));
     if(!board_id || board_id.includes("quicksaveboard")) {
@@ -30,5 +30,21 @@ else if("https://offerup.com/boards/" == window.location) {
       .attr("link", apiUrl)
       .click(removeFromBoard);
     $(this).prepend(div);
+  });
+}
+
+else if(isDetailPage()) {
+  $("table").siblings("img").show();
+  $("table").siblings("img[src*='left']")[0].onclick = (function(e){
+    var row = document.getElementsByTagName("table")[0].getElementsByTagName('tr')[0];
+    var cells = row.getElementsByTagName('td');
+    for(i = cells.length - 2; i >= 0; i--)
+      row.insertBefore(row.removeChild(cells[i == cells.length ? 0 : i + 1]), cells[i]);
+  });
+  $("table").siblings("img[src*='right']")[0].onclick = (function(e){
+    var row = document.getElementsByTagName("table")[0].getElementsByTagName('tr')[0];
+    var cells = row.getElementsByTagName('td');
+    for(i = 0; i < cells.length - 1; i++)
+      row.insertBefore(row.removeChild(cells[i == cells.length ? 0 : i + 1]), cells[i]);
   });
 }
